fa103c0d4941374d829861490ecb7591dbdac506
Dayro Martínez <djmd96@hotmail.com>
2022-01-06 10:25:13 -0500

    Estructura general backend

---
+++ randomgenerate.co/src/test/java/com/sofka/randomgenerate/co/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.randomgenerate.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
